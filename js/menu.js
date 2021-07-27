8a40f88fe680a8f88c1a8e2738d2627e5933e657
Chris De Lima <delimac@hey.com>
2021-07-27 21:12:19 +0800

    added open and close menu toggle

--- js/menu.js
+++ js/menu.js
@@ -2,6 +2,15 @@ const toggleTag = document.querySelector("a.toggle-nav")
 const mainTag = document.querySelector("main")
 
 // when clicking the toggle tag, toggle a class of open on the main tag
+// once open, make toggle text say Close
+// if closed, make toggle text say Menu
 toggleTag.addEventListener("click", function () {
   mainTag.classList.toggle("open")
+
+  if (mainTag.classList.contains("open")) {
+    toggleTag.innerHTML = `<img src="../img/close.svg"> Close`
+  } else {
+    toggleTag.innerHTML = `<img src="../img/menu.svg"> Menu`
+  }
+
 })
