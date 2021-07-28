13ce3d30eb22b869976d05882979edd99acab81d
Chris De Lima <delimac@hey.com>
2021-07-28 21:53:31 +0800

    added layering in HTML, created slides js

---
+++ js/slides.js
@@ -0,0 +1,16 @@
+// pick all images and layer them based on z-index
+const slideArea = document.querySelector("div.slides")
+const images = slideArea.querySelectorAll("img")
+
+// we want to keep track of two things
+let currentSlide = 0
+let z = 1
+
+// when user clicks slideArea, change slide based on z-index
+slideArea.addEventListener("click", function () {
+  currentSlide = currentSlide + 1
+  z = z + 1
+
+  // pick the right image
+  images[currentSlide].style.zIndex = z
+})
