7a3bc779ab9e92700bc0528f64d63e0769701873
Chris De Lima <delimac@hey.com>
2021-07-28 22:11:17 +0800

    added loop in the images

--- js/slides.js
+++ js/slides.js
@@ -9,6 +9,11 @@ let z = 1
 // when user clicks slideArea, change slide based on z-index
 slideArea.addEventListener("click", function () {
   currentSlide = currentSlide + 1
+
+  if (currentSlide > images.length - 1) {
+    currentSlide = 0
+  }
+
   z = z + 1
 
   // pick the right image
